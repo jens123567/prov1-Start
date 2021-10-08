afa64938007e66267a02b530bf1206bc27fe3aeb
Karye <karye.webb@gmail.com>
2021-10-08 17:43:10 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Text.RegularExpressions;
+
+namespace Prov1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Gästboksfilen
+        string filen = "./kontakter.tsv";
+
+        // Kontaktlistan
+        List<string> kontaktLista = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Lägg fokus i första inmatningsrutan
+            rutaNamn.Focus();
+
+        }
+
+
+    }
+}
